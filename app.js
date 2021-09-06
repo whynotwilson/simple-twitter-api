636661ab5a3ae88e1f7ec4ec8d5c380d86043459
Wilson Tseng <p780729@hotmail.com>
2021-09-06 13:45:39 +0800

    移除 bodyParser
    改用原生 express.json

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const helpers = require('./_helpers');
 
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const app = express()
@@ -12,6 +11,10 @@ app.use(methodOverride('_method'))
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
